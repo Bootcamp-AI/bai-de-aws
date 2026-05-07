c5ea20fe2d1c546d3a1ea0bebbd6298d14ac4f05
BootcampAI <57165250+Bootcamp-AI@users.noreply.github.com>
2026-05-07 01:27:47 -0500

    Update devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,9 +2,10 @@
 set -e
 
 echo "==> Instalando dependencias Python..."
-pip install -r requirements.txt
+pip install -r /workspaces/bai-de-aws/.devcontainer/requirements.txt
 
 echo "==> Iniciando contenedor Sakila..."
 docker start sakila || docker run -d -p 3306:3306 --name sakila -e MYSQL_ROOT_PASSWORD=password dbaden/sakila
 
 echo "==> Listo!"
+
